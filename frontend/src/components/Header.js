a1003b57cff950bf9546c268e61475621e5f58ad
joaquinrapetti <j.rapetti92@gmail.com>
2022-03-16 12:02:17 -0300

    fix all with linter

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
 
 const navbarStyle = {
-    backgroundColor: 'lightblue'
+  backgroundColor: 'lightblue',
 };
 
 const Header = ({ title }) => {
-    // const { title } = props;
-    return (
-        <Navbar style={navbarStyle} variant="light">
-            <Container>
-                <Navbar.Brand href="/">{title}</Navbar.Brand>
-            </Container>
-        </Navbar>
-    )
+  // const { title } = props;
+  return (
+    <Navbar style={navbarStyle} variant="light">
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
